42a3a09114f91500c2d9a544af1ae30e0b5b38a4
himanshu-flance <himanshu.flance@gmail.com>
2026-07-15 19:36:57 +0530

    changed app.py

--- app.py
+++ app.py
@@ -2,12 +2,23 @@ from flask import Flask
 
 
 def create_app():
+    x=10
+    y=10
     app = Flask(__name__)
     print("inside create_app function")
 
     @app.route('/')
     def home():
         print("inside home function")
+        return 'Hi hi GFG43 hi 12378'
+    
+    @app.route('/test')
+    def test():
+        print("inside home function")
+        x=5
+        while x>1:
+            print(x)
+        break
         return 'Hi hi GFG43 hi 12378'
 
     return app
